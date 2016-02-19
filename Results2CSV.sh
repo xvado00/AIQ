e5425a861ebcd6bc75f130c3e2d2c5e321cf4c10
Ondřej Vadinský <ondrej.vadinsky@vse.cz>
2016-02-19 14:22:52 +0100

    Standard Deviation: Compute and print standard deviation of results.

--- Results2CSV.sh
+++ Results2CSV.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # BASH script to convert ComuteFromLog output to a CSV format
-tail -n +4 $1 | sed s/\ +\\/-\ /,/ | sed s/\ :/,\ / | sed s/\ \ log\\//,\ / | \
+tail -n +4 $1 | sed s/\ +\\/-\ /,/ | sed s/\ SD\ /,/ | sed s/\ :/,\ / | sed s/\ \ log\\//,\ / | \
                 sed s/_/\ / | sed s/_/,\ / | sed s/_/,\ / | \
                 tr '(' ',' | tr ')' ',' | sed s/,_.*// | cut -c9-
